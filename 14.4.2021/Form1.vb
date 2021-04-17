b66df94d9b74b8e6751101c2e9e9df146f1a88c2
Irfan Zuhairi Bin Jasni <18DDT19F1107@student.ptss.edu.my>
2021-04-17 12:11:25 +0800

    Add project files.

---
+++ 14.4.2021/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub lblNamaHari_Click(sender As Object, e As EventArgs) Handles lblNamaHari.Click
+        Dim strNamaHari(6) As String
+
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+
+        lblNamaHari.Text = strNamaHari(1)
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strNamaHari() As String = {"Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"}
+
+        Dim i As Integer
+
+        Array.Reverse(strNamaHari)
+        For i = 0 To 6
+            lstbox.Items.Add(strNamaHari(i))
+        Next
+    End Sub
+
+End Class
